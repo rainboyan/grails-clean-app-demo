3a397429537180ee23f0f85e4baec0449bd38ad7
Michael Yan <rain@rainboyan.com>
2022-03-30 23:44:53 +0800

    Add metrics spring boot startup time

--- grails-app/init/org/grails/demo/Application.groovy
+++ grails-app/init/org/grails/demo/Application.groovy
@@ -5,11 +5,23 @@ import grails.boot.config.GrailsAutoConfiguration
 
 import groovy.transform.CompileStatic
 import org.springframework.context.annotation.ComponentScan
+import org.springframework.boot.context.metrics.buffering.BufferingApplicationStartup
 
 @CompileStatic
 @ComponentScan
 class Application extends GrailsAutoConfiguration {
     static void main(String[] args) {
-        GrailsApp.run(Application, args)
+         def now = System.currentTimeMillis()
+
+        BufferingApplicationStartup startup = new BufferingApplicationStartup(2048)
+        startup.addFilter(startupStep -> startupStep.getName().matches("spring.beans.instantiate"))
+
+        GrailsApp grailsApp = new GrailsApp(Application)
+        grailsApp.setApplicationStartup(startup)
+        grailsApp.enableBeanCreationProfiler = true
+
+        grailsApp.run(args)
+
+        println "${System.currentTimeMillis()-now}ms"
     }
 }
